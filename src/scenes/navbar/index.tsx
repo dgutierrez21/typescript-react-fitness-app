00ecb2c8654993e6f274581653c7a26def0c153e
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-07 20:15:24 -0600

    Action button

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -5,6 +5,7 @@ import Logo from "@/assets/logo.png";
 import { Link } from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import { ActionButton } from "@/shared/ActionButton";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -53,7 +54,9 @@ export const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
 
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button>Become a Member</button>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
                 </div>
               </div>
             ) : (
