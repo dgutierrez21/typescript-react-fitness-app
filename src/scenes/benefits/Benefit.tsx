0f38a5948815e10d2363eed0dcd1eb33b66eb278
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-10 20:53:14 -0600

    add animations to header and benefits

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -1,13 +1,22 @@
 import { BenefitType, SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = BenefitType & {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
+const childVariant = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+};
+
 const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
   return (
-    <div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center">
+    <motion.div
+      className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center"
+      variants={childVariant}
+    >
       <div className="mb-4 flex justify-center">
         <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
           {icon}
@@ -24,7 +33,7 @@ const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
       >
         <p>Learn More</p>
       </AnchorLink>
-    </div>
+    </motion.div>
   );
 };
 
