4e5c6810876d7d42d5d5ba3f741eda2f95a313b6
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-12 17:42:57 -0600

    add form | deploy react hook form | deploy formSubmit

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -10,6 +10,26 @@ type Props = {
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
+  const {
+    register,
+    trigger,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+      return;
+    }
+
+    reset();
+  };
+
+  const inputStyles =
+    "mt-5 w-full rounded-lg bg-primary-500 px-5 py-3 placeholder-white";
+
   return (
     <section className="mx-auto w-5/6 pb-32 pt-24" id="contactus">
       <motion.div
@@ -33,6 +53,87 @@ const ContactUs = ({ setSelectedPage }: Props) => {
           leo egestas nisi elit risus sit. Nunc cursus sagittis.
         </p>
       </motion.div>
+
+      {/* form and image */}
+      <div className="mt-10 justify-between gap-8 md:flex">
+        <motion.div
+          className="mt-10 basis-3/5 md:mt-0"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <form
+            action="https://formsubmit.co/cyrn.riexz61@padye.com"
+            method="POST"
+            target="_blank"
+            onSubmit={onSubmit}
+          >
+            <input
+              type="text"
+              className={inputStyles}
+              placeholder="NAME"
+              {...register("name", {
+                required: true,
+                maxLength: 100,
+              })}
+            />
+            {errors.name && (
+              <p className="mt-1 text-primary-500">
+                {errors.name.type === "required" && "This field is required."}
+                {errors.name.type === "maxLength" && "Max length is 100 char."}
+              </p>
+            )}
+
+            <input
+              type="text"
+              className={inputStyles}
+              placeholder="EMAIL"
+              {...register("email", {
+                required: true,
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              })}
+            />
+            {errors.email && (
+              <p className="mt-1 text-primary-500">
+                {errors.email.type === "required" && "This field is required."}
+                {errors.email.type === "pattern" && "Invalid email address."}
+              </p>
+            )}
+
+            <textarea
+              rows={4}
+              cols={50}
+              style={{ resize: "none" }}
+              className={inputStyles}
+              placeholder="MESSAGE"
+              {...register("message", {
+                required: true,
+                maxLength: 2000,
+              })}
+            />
+            {errors.message && (
+              <p className="mt-1 text-primary-500">
+                {errors.message.type === "required" &&
+                  "This field is required."}
+                {errors.message.type === "maxLength" &&
+                  "Max length is 2000 char."}
+              </p>
+            )}
+
+            <button
+              type="submit"
+              className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+            >
+              SUBMIT
+            </button>
+          </form>
+        </motion.div>
+      </div>
     </section>
   );
 };
