a412fa2de4134f1183ec640e9244828c21384495
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-12 20:27:14 -0600

    add div for animations and add comments for sections

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -32,108 +32,112 @@ const ContactUs = ({ setSelectedPage }: Props) => {
 
   return (
     <section className="mx-auto w-5/6 pb-32 pt-24" id="contactus">
-      <motion.div
-        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.5 }}
-        transition={{ delay: 0.2, duration: 0.5 }}
-        variants={{
-          hidden: { opacity: 0, x: -50 },
-          visible: { opacity: 1, x: 0 },
-        }}
-        className="md:w-3/5"
-      >
-        <HText>
-          <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
-        </HText>
-        <p className="my-5">
-          Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
-          sapien vel rhoncus. Placerat at in enim pellentesque. Nulla adipiscing
-          leo egestas nisi elit risus sit. Nunc cursus sagittis.
-        </p>
-      </motion.div>
-
-      {/* form and image */}
-      <div className="mt-10 justify-between gap-8 md:flex">
+      <motion.div>
+        {/* header */}
         <motion.div
-          className="mt-10 basis-3/5 md:mt-0"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
           transition={{ delay: 0.2, duration: 0.5 }}
           variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: { opacity: 1, y: 0 },
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
           }}
+          className="md:w-3/5"
         >
-          <form
-            action="https://formsubmit.co/cyrn.riexz61@padye.com"
-            method="POST"
-            target="_blank"
-            onSubmit={onSubmit}
-          >
-            <input
-              type="text"
-              className={inputStyles}
-              placeholder="NAME"
-              {...register("name", {
-                required: true,
-                maxLength: 100,
-              })}
-            />
-            {errors.name && (
-              <p className="mt-1 text-primary-500">
-                {errors.name.type === "required" && "This field is required."}
-                {errors.name.type === "maxLength" && "Max length is 100 char."}
-              </p>
-            )}
-
-            <input
-              type="text"
-              className={inputStyles}
-              placeholder="EMAIL"
-              {...register("email", {
-                required: true,
-                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              })}
-            />
-            {errors.email && (
-              <p className="mt-1 text-primary-500">
-                {errors.email.type === "required" && "This field is required."}
-                {errors.email.type === "pattern" && "Invalid email address."}
-              </p>
-            )}
-
-            <textarea
-              rows={4}
-              cols={50}
-              style={{ resize: "none" }}
-              className={inputStyles}
-              placeholder="MESSAGE"
-              {...register("message", {
-                required: true,
-                maxLength: 2000,
-              })}
-            />
-            {errors.message && (
-              <p className="mt-1 text-primary-500">
-                {errors.message.type === "required" &&
-                  "This field is required."}
-                {errors.message.type === "maxLength" &&
-                  "Max length is 2000 char."}
-              </p>
-            )}
-
-            <button
-              type="submit"
-              className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
-            >
-              SUBMIT
-            </button>
-          </form>
+          <HText>
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+          </HText>
+          <p className="my-5">
+            Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
+            sapien vel rhoncus. Placerat at in enim pellentesque. Nulla
+            adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
+          </p>
         </motion.div>
-      </div>
+
+        {/* form and image */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form
+              action="https://formsubmit.co/cyrn.riexz61@padye.com"
+              method="POST"
+              target="_blank"
+              onSubmit={onSubmit}
+            >
+              <input
+                type="text"
+                className={inputStyles}
+                placeholder="NAME"
+                {...register("name", {
+                  required: true,
+                  maxLength: 100,
+                })}
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required."}
+                  {errors.name.type === "maxLength" &&
+                    "Max length is 100 char."}
+                </p>
+              )}
+
+              <input
+                type="text"
+                className={inputStyles}
+                placeholder="EMAIL"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+
+              <textarea
+                rows={4}
+                cols={50}
+                style={{ resize: "none" }}
+                className={inputStyles}
+                placeholder="MESSAGE"
+                {...register("message", {
+                  required: true,
+                  maxLength: 2000,
+                })}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required."}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 char."}
+                </p>
+              )}
+
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+              >
+                SUBMIT
+              </button>
+            </form>
+          </motion.div>
+        </div>
+      </motion.div>
     </section>
   );
 };
